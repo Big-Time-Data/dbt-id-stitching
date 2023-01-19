25e093eb5bb5e1a697887e6627ef92022f75fe14
Fritz Larco <flarco@live.com>
2023-01-19 07:59:38 -0300

    add edge_rank_id

--- models/edges.sql
+++ models/edges.sql
@@ -5,13 +5,14 @@
     {% set sql_statements = dbt_utils.get_column_values(table=ref('queries'), column='sql_to_run') %}
 
     SELECT
-        ROW_NUMBER() OVER (ORDER BY 1 DESC) AS rudder_id,
-        ROW_NUMBER() OVER (ORDER BY 1 DESC) AS original_rudder_id,
+        ROW_NUMBER() OVER (ORDER BY edge_rank_id ASC) AS rudder_id,
+        ROW_NUMBER() OVER (ORDER BY edge_rank_id ASC) AS original_rudder_id,
         edge_a,
         edge_a_label,
         edge_b,
         edge_b_label,
-        edge_timestamp
+        edge_timestamp,
+        edge_rank_id
     FROM (
         {{ ' UNION '.join(sql_statements) }}
     ) AS s
@@ -25,7 +26,8 @@
     cte_timestamp AS (
         SELECT
             rudder_id,
-            MAX(edge_timestamp) AS edge_timestamp
+            MIN(edge_timestamp) AS edge_timestamp,
+            MIN(edge_rank_id) AS edge_rank_id
         FROM {{ this }}
         GROUP BY rudder_id
     ),
@@ -33,7 +35,8 @@
     cte_min_edge_1 AS (
         select 
           t.*,
-          cte_timestamp.edge_timestamp
+          cte_timestamp.edge_timestamp,
+          cte_timestamp.edge_rank_id
         from (
           SELECT
               edge,
@@ -59,11 +62,13 @@
         SELECT
             edge,
             MIN(rudder_id) AS first_row_id,
-            max(edge_timestamp) as edge_timestamp
+            MIN(edge_timestamp) as edge_timestamp,
+            MIN(edge_timestamp) as edge_rank_id
         FROM (
             SELECT
                 LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
                 LEAST(a.edge_timestamp, b.edge_timestamp) AS edge_timestamp,
+                LEAST(a.edge_rank_id, b.edge_rank_id) AS edge_rank_id,
                 LEAST(o.edge_a) AS edge
             FROM {{ this }} AS o
             LEFT OUTER JOIN cte_min_edge_1 AS a
@@ -74,6 +79,7 @@
             SELECT
                 LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
                 LEAST(a.edge_timestamp, b.edge_timestamp) AS edge_timestamp,
+                LEAST(a.edge_rank_id, b.edge_rank_id) AS edge_rank_id,
                 LOWER(o.edge_b) AS edge
             FROM {{ this }} AS o
             LEFT OUTER JOIN cte_min_edge_1 AS a
@@ -89,11 +95,13 @@
         SELECT
             edge,
             MIN(rudder_id) AS first_row_id,
-            max(edge_timestamp) as edge_timestamp
+            MIN(edge_timestamp) as edge_timestamp,
+            MIN(edge_timestamp) as edge_rank_id
         FROM (
             SELECT
                 LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
                 LEAST(a.edge_timestamp, b.edge_timestamp) AS edge_timestamp,
+                LEAST(a.edge_rank_id, b.edge_rank_id) AS edge_rank_id,
                 LOWER(o.edge_a) AS edge
             FROM {{ this }} AS o
             LEFT OUTER JOIN cte_min_edge_2 AS a
@@ -104,6 +112,7 @@
             SELECT
                 LEAST(a.first_row_id, b.first_row_id) AS rudder_id,
                 LEAST(a.edge_timestamp, b.edge_timestamp) AS edge_timestamp,
+                LEAST(a.edge_rank_id, b.edge_rank_id) AS edge_rank_id,
                 LOWER(o.edge_b) AS edge
             FROM {{ this }} AS o
             LEFT OUTER JOIN cte_min_edge_2 AS a
@@ -119,7 +128,8 @@
         SELECT
             o.original_rudder_id,
             LEAST(a.first_row_id, b.first_row_id) AS new_rudder_id,
-            LEAST(a.edge_timestamp, b.edge_timestamp) AS edge_timestamp
+            LEAST(a.edge_timestamp, b.edge_timestamp) AS edge_timestamp,
+            LEAST(a.edge_rank_id, b.edge_rank_id) AS edge_rank_id
         FROM {{ this }} AS o
         LEFT OUTER JOIN cte_min_edge_3 AS a
             ON LOWER(o.edge_a) = a.edge
@@ -134,7 +144,8 @@
         e.edge_a_label,
         e.edge_b,
         e.edge_b_label,
-        cte_new_id.edge_timestamp AS edge_timestamp
+        cte_new_id.edge_timestamp AS edge_timestamp,
+        cte_new_id.edge_rank_id AS edge_rank_id
     FROM {{ this }} AS e
     INNER JOIN cte_new_id
         ON e.original_rudder_id = cte_new_id.original_rudder_id
